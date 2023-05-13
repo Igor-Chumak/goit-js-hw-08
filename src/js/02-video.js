61beca1944f623a4fc86910a8f6ed35a0073f28a
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-05-13 17:18:56 +0300

    task 2 +npm video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+// npm install @vimeo/player
+import Player from '@vimeo/player';
 
+    const iframe = document.querySelector('iframe');
+    // const player = new Vimeo.Player(iframe);
+    const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
