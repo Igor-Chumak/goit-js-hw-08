9edca96e165932d4d5c5fbabb7030bbdb4497ac9
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-05-13 16:50:32 +0300

    del comment

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,10 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 // console.log(galleryItems);
-// Описан в документации
 // npm install simplelightbox;
+// Change code below this line
+
 import SimpleLightbox from "simplelightbox";
-// Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 document
