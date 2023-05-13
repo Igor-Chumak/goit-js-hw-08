a7a15ef0941ca211fd09c8501a0e03c828a65612
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-05-13 16:18:31 +0300

    npm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,12 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 // console.log(galleryItems);
+// Описан в документации
+// npm install simplelightbox;
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 document
   .querySelector(".gallery")
