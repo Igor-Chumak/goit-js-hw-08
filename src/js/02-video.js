cb027f2bcd6e2ba21b26033a13f1eb84d97af3c4
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-05-14 00:42:22 +0300

    2 ok no lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,14 +2,20 @@
 import Player from '@vimeo/player';
 
     const iframe = document.querySelector('iframe');
-    // const player = new Vimeo.Player(iframe);
     const player = new Player(iframe);
-    let currentTime=0;
+    // const player = new Vimeo.Player(iframe);
+    let currentTime=localStorage.getItem("videoplayer-current-time");
+        console.log('Считано время: ', currentTime);
 
     player.on('timeupdate', function({seconds}) {
         currentTime = seconds;
-        console.log(currentTime);
+        localStorage.setItem("videoplayer-current-time", currentTime);
+        console.log('currentTime: ', currentTime);
     });
+
+    player.setCurrentTime(currentTime).then(function(seconds) {
+        console.log('Play will start from seconds: ', seconds);
+            });
 
     player.getVideoTitle().then(function(title) {
         console.log('title:', title);
