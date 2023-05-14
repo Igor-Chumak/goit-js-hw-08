6c435cf88068a48caf211cc6b271832ad9c8d841
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-05-14 12:44:34 +0300

    02 ok

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,30 +3,22 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-
     const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
-    // const player = new Vimeo.Player(iframe);
+
     let currentTime=localStorage.getItem("videoplayer-current-time");
-        console.log('Считано время: ', currentTime);
+        // console.log('Считано время: ', currentTime);
 
-    // player.on('timeupdate', _.throttle(function() {}, 0));
-    player.on('timeupdate',  _.throttle(timeupdate, 0));
-
-    function timeupdate(e) {
-        console.log(e);
-    }
-
-    // player.on('timeupdate', _.throttle(function({seconds}) {
-    //     currentTime = seconds;
-    //     localStorage.setItem("videoplayer-current-time", currentTime);
-    //     console.log('currentTime: ', currentTime);
-    // }, 1000));
+    player.on('timeupdate', throttle(function({seconds}) {
+        currentTime = seconds;
+        localStorage.setItem("videoplayer-current-time", currentTime);
+        // console.log('currentTime: ', currentTime);
+    }, 1000));
 
     player.setCurrentTime(currentTime).then(function(seconds) {
-        console.log('Play will start from seconds: ', seconds);
+        // console.log('Play will start from seconds: ', seconds);
             });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
