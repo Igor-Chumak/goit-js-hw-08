b2904ac578735034548ae9e033da9a290443e5e1
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-05-14 12:06:04 +0300

    from mentor

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,8 @@
 // npm install @vimeo/player
-// npm i -g npm
 // npm i --save lodash.throttle
 import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-// import lodashThrottle from 'lodash.throttle';
-var _ = require('lodash.throttle');
-var throttle = require('lodash.throttle');
-// var lodashThrottle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
+
 
     const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
