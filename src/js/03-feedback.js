62def216280cad9a0eabbad39cae1f4c44d43524
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-05-14 19:21:20 +0300

    3 ok

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,29 +2,21 @@ import throttle from 'lodash.throttle';
 
 const ref = {
   form: document.querySelector('.feedback-form'),
-  inputEmail: document.querySelector('input[name="email"]'),
-  inputMessage: document.querySelector('textarea[name="message"]'),
 };
-let feedbackFormState = {};
-//  feedbackFormState = {
-//     email: 'chigorv@ukr.net',
-//     message: 'test',
-// };
-// localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState));
-
-feedbackFormState = JSON.parse(localStorage.getItem('feedback-form-state'));
-console.log('from: ', feedbackFormState);
-const { email: feedbackEmail = '', message: feedbackMmessage = '' } = feedbackFormState;
-ref.inputEmail.value = feedbackEmail;
-ref.inputMessage.value = feedbackMmessage;
 
 ref.form.addEventListener('input', throttle(handleDataInput, 500));
 ref.form.addEventListener('submit', handleSubmitOutput);
 
-// function
+let feedbackFormState = localStorage.getItem('feedback-form-state');
+feedbackFormState = feedbackFormState === null ? {} : JSON.parse(feedbackFormState);
+
+const { email: feedbackEmail = '', message: feedbackMessage = '' } = feedbackFormState;
+ref.form.elements.email.value = feedbackEmail;
+ref.form.elements.message.value = feedbackMessage;
 
 function handleDataInput(e) {
-  console.log(e.target.name);
+  feedbackFormState[e.target.name] = e.target.value;
+  localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState));
 }
 
 function handleSubmitOutput(e) {
@@ -32,11 +24,8 @@ function handleSubmitOutput(e) {
   const {
     elements: { email, message },
   } = e.currentTarget;
-  console.log({ email: email.value.trim(), message: message.value });
+  console.log({ email: email.value, message: message.value });
   email.value = '';
   message.value = '';
   localStorage.removeItem("feedback-form-state");
-  console.log('submit', localStorage.getItem('feedback-form-state'));
 }
-
-// chigorv@ukr.net
